4c3add18b04b8cd5e3da900375865405a4be0b44
ashleyromano <ashleye.romano@gmail.com>
2022-05-03 18:51:13 -0400

    updated models

--- models/order.js
+++ models/order.js
@@ -6,13 +6,15 @@ const orderSchema = new Schema({
     id: String,
     entrepreneur_id: String,
     customer_id: String,
-    date: Date,
+    date: { type: Date, default: Date.now },
     currency: String,
     total_price: Number,
     amount_paid: Number,
     product_id: String,
     full_pay: Boolean,
-    loan_id: String
+    loan_id: String,
+    warehouse_id: String,
+    purchase_reasons: String,
 
 })
 
