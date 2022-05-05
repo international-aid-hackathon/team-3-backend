c5407c79f178a18e91c24a67a619468bdb9dfc7c
oraweechan <89260194+oraweechan@users.noreply.github.com>
2022-05-05 01:50:05 -0700

    added route to create job from user id

--- routes/jobs.js
+++ routes/jobs.js
@@ -6,7 +6,7 @@ const router = Router()
 
 /*---------- Public Routes ----------*/
 router.get('/', jobCtrl.index)
-// router.post('/', jobCtrl.create)
+router.post('/:id', jobCtrl.create)
 // router.get('/:id', jobCtrl.show)
 router.get('/:id', jobCtrl.findByUser)
 router.put('/:id', jobCtrl.update)
@@ -14,7 +14,7 @@ router.delete('/:id', jobCtrl.destroy)
 
 
 /*---------- Protected Routes ----------*/
-router.use(decodeUserFromToken)
-router.post('/', checkAuth, jobCtrl.create)
+// router.use(decodeUserFromToken)
+// router.post('/', checkAuth, jobCtrl.create)
 
 export { router }
